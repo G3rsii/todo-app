976e3eb47e20bd6a36661d75bb3ad24ae55cfd0c
gersii <gersisalias@gmail.com>
2024-08-10 22:49:41 +0200

    MongoDB

--- client/src/components/Input/TagInput.jsx
+++ client/src/components/Input/TagInput.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 import { MdAdd, MdClose } from "react-icons/md";
-const TagInput = () => {
 
-  const [inputValue, setinputValue] = useState("");
+const TagInput = ({tags, setTags}) => {
+
+ 
+  const [inputValue, setInputValue] = useState("");
   const handleInputChange = (e) => {
-    setinputValue(e.target.value);
+    setInputValue(e.target.value);
   }
   const addNewTag = () => {
     if (inputValue.trim() !== "") {
       setTags([...tags, inputValue.trim()]);
-      setinputValue("");
+      setInputValue("");
     }
   }
 
@@ -28,7 +30,7 @@ const TagInput = () => {
       {tags?.length > 0 && (
         <div className='flex items-center gap-2 flex-wrap mt-2'>
           {tags.map((tag, index) => (
-            <span key={index} className=''>
+            <span key={index} className='flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded'>
               # {tag}
               <button onClick={() => { handleRemoveTag(tag) }}>
                 <MdClose />
@@ -39,6 +41,7 @@ const TagInput = () => {
       )}
       <div className='flex items-center gap-4 mt-3'>
         <input type="text"
+               value={inputValue}
           className='text-sm bg-trasparent border px-3 py-2 rounded outline-none'
           placeholder='Add tags'
           onChange={handleInputChange}
